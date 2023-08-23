633650208fc290185e79f6710333c63a226fcf6b
Theologos Mavropoulos <rusinner88@gmail.com>
2023-08-23 16:40:22 +0300

    final

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongo = require("mongodb");
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db("freecodecamp-urlshortener");
+const urls = db.collection("urls");
 const bodyParser = require("body-parser");
+const urlParser = require("url");
+const dns = require("dns");
 const app = express();
 
 // Basic Configuration
@@ -11,6 +15,8 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
@@ -19,8 +25,31 @@ app.get("/", function (req, res) {
 });
 
 // Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.post("/api/shorturl", function (req, res) {
+  console.log(req.body);
+  const url = req.body.url;
+  const dnsLookup = dns.lookup(
+    urlParser.parse(url).hostname,
+    async (err, address) => {
+      if (!address) {
+        res.json({ error: "Invalid url" });
+      } else {
+        const urlCount = await urls.countDocuments({});
+        const urlDoc = {
+          url,
+          short_url: urlCount,
+        };
+        const result = await urls.insertOne(urlDoc);
+        console.log(result);
+        res.json({ original_url: url, short_url: urlCount });
+      }
+    }
+  );
+});
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  const shorturl = req.params.short_url;
+  const urlDoc = await urls.findOne({ short_url: +shorturl });
+  res.redirect(urlDoc.url);
 });
 
 app.listen(port, function () {
